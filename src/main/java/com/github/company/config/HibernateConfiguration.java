ad3bc0052d4744fd99c6b0298da72a53295659e7
EcstasyAwesome <ecstasy.awesome@gmail.com>
2018-03-31 14:14:47 +0300

    update

--- src/main/java/com/github/company/config/HibernateConfiguration.java
+++ src/main/java/com/github/company/config/HibernateConfiguration.java
@@ -1,24 +1,61 @@
 package com.github.company.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+import java.util.TimeZone;
 
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfiguration {
 
-    @Value("classpath:hibernate.cfg.xml")
-    private Resource config;
+    @Autowired
+    private Environment env;
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory(){
+    public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setConfigLocation(config);
+        sessionFactory.getObject().withOptions().jdbcTimeZone(TimeZone.getTimeZone("UTC"));
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setHibernateProperties(hibernateProperties());
         sessionFactory.setAnnotatedPackages("com.github.company.dao.entity");
         return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(env.getProperty("ds.url"));
+        dataSource.setUsername(env.getProperty("ds.username"));
+        dataSource.setPassword(env.getProperty("ds.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private Properties hibernateProperties() {
+        String hbm2ddl = "hibernate.hbm2ddl.auto";
+        String dialect = "hibernate.dialect";
+        String showSql = "hibernate.show_sql";
+        Properties prop = new Properties();
+        prop.setProperty(hbm2ddl, env.getProperty(hbm2ddl));
+        prop.setProperty(dialect, env.getProperty(dialect));
+        prop.setProperty(showSql, env.getProperty(showSql));
+        return prop;
     }
 }
--- src/main/java/com/github/company/config/WebAppConfig.java
+++ src/main/java/com/github/company/config/WebAppConfig.java
@@ -2,11 +2,13 @@ package com.github.company.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.*;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.github.company")
+@PropertySource("classpath:configuration.properties")
 public class WebAppConfig implements WebMvcConfigurer {
 
     @Override
